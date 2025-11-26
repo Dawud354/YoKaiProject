e65436b3e10d9d425a611f7a17a2e6f1ab018d53
Dawud354 <dawud.a.hussain@gmail.com>
2025-11-26 13:42:14 +0000

    Feat: Create MoveResult class to help return results to frontEnd

---
+++ src/main/java/com/github/Dawud354/BattleLogic/MoveResult.java
@@ -0,0 +1,26 @@
+package com.github.Dawud354.BattleLogic;
+
+public class MoveResult {
+
+    private String user;
+    private String target;
+    private int damage;
+
+    public MoveResult(String user, String target, int damage) {
+        this.target = target;
+        this.user = user;
+        this.damage = damage;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+}
