1afe9e3075ff65eb716259b841e34c848585b10d
Dawud354 <dawud.a.hussain@gmail.com>
2025-11-27 08:54:11 +0000

    Feat: Add more data to MoveResult
    Add moveName, remainingHealth, isUserDead

--- src/main/java/com/github/Dawud354/BattleLogic/MoveResult.java
+++ src/main/java/com/github/Dawud354/BattleLogic/MoveResult.java
@@ -4,12 +4,17 @@ public class MoveResult {
 
     private String user;
     private String target;
+    private String moveName;
     private int damage;
+    private int remainingHealth;
+    private boolean isUserDead = false;
 
-    public MoveResult(String user, String target, int damage) {
+    public MoveResult(String user, String target, String moveName, int damage, int remainingHealth) {
         this.target = target;
         this.user = user;
+        this.moveName = moveName;
         this.damage = damage;
+        this.remainingHealth = remainingHealth;
     }
 
     public String getUser() {
@@ -26,5 +31,29 @@ public class MoveResult {
 
     public void setDamage(int damage) {
         this.damage = damage;
+    }
+
+    public String getMoveName() {
+        return moveName;
+    }
+
+    public void setMoveName(String moveName) {
+        this.moveName = moveName;
+    }
+
+    public void setIsUserDead(boolean isUserDead) {
+        this.isUserDead = isUserDead;
+    }
+
+    public boolean getIsUserDead() {
+        return isUserDead;
+    }
+
+    public int getRemainingHealth() {
+        return remainingHealth;
+    }
+
+    public void setRemainingHealth(int remainingHealth) {
+        this.remainingHealth = remainingHealth;
     }
 }
