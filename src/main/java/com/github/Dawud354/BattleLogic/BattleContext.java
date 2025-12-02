7a471f63c3aa42ab91a5d32f70836b00e8f149ed
Dawud354 <dawud.a.hussain@gmail.com>
2025-12-02 11:04:01 +0000

    Feat: Create a BatteContext class
    This is designed to hold data relating to the state of the battle

---
+++ src/main/java/com/github/Dawud354/BattleLogic/BattleContext.java
@@ -0,0 +1,34 @@
+package com.github.Dawud354.BattleLogic;
+
+import com.github.Dawud354.Team.*;
+
+public class BattleContext {
+    private PlayerTeam playerTeam;
+    private EnemyTeam enemyTeam;
+    private int turnCount = 1;
+
+    public BattleContext(PlayerTeam playerTeam, EnemyTeam enemyTeam) {
+        if (playerTeam == null || enemyTeam == null) {
+            throw new IllegalArgumentException("Teams must be provided");
+        }
+        this.playerTeam = playerTeam;
+        this.enemyTeam = enemyTeam;
+    }
+
+    public PlayerTeam getPlayerTeam() {
+        return playerTeam;
+    }
+
+    public EnemyTeam getEnemyTeam() {
+        return enemyTeam;
+    }
+
+    public int getTurnCount() {
+        return turnCount;
+    }
+
+    public void incrementTurnCount() {
+        turnCount++;
+    }
+
+}
