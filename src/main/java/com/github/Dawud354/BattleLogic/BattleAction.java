cf088fd530b17adb69404c6ce98d5c63e728590a
Dawud354 <dawud.a.hussain@gmail.com>
2025-12-02 11:09:05 +0000

    Feat: BattleAction implement comparable

--- src/main/java/com/github/Dawud354/BattleLogic/BattleAction.java
+++ src/main/java/com/github/Dawud354/BattleLogic/BattleAction.java
@@ -2,7 +2,7 @@ package com.github.Dawud354.BattleLogic;
 
 import com.github.Dawud354.YoKaiCode.YoKai;
 
-public abstract class BattleAction {
+public abstract class BattleAction implements Comparable<BattleAction> {
     private YoKai user;
     private int priority;
 
@@ -18,5 +18,11 @@ public abstract class BattleAction {
         return priority;
     }
 
+    @Override
+    public int compareTo(BattleAction action) {
+        return Integer.compare(action.priority, this.priority);
+    } // END compareTo
+
     public abstract void execute();
+
 }
