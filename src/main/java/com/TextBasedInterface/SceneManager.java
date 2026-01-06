c1cb0095d32692ea3eeb1a5149dde7d0995e0c30
Dawud354 <dawud.a.hussain@gmail.com>
2026-01-06 15:51:20 +0000

    Remove: Remove test code from testing battle logic

--- src/main/java/com/TextBasedInterface/SceneManager.java
+++ src/main/java/com/TextBasedInterface/SceneManager.java
@@ -2,6 +2,7 @@ package com.TextBasedInterface;
 
 import java.util.Random;
 
+import com.github.Dawud354.PlayerSave.PlayerSave;
 import com.github.Dawud354.Team.EnemyTeam;
 import com.github.Dawud354.Team.PlayerTeam;
 import com.github.Dawud354.YoKaiCode.PhysicalMove;
@@ -16,35 +17,9 @@ public class SceneManager {
     }
 
     public static void goBattle() throws InterruptedException{
-        PhysicalMove pm ;
-        SpecialMove sm ;
-        YoKai y;
-        String[] physicalMoveNames = {"Slash", "Punch","Box","Rocket Punch"};
-        String[] specialMoveNames = {"Fireball", "Ice Blast", "Thunder Strike", "Earthquake"};
-        String[] ykNames = {"Jibanyan", "Komasan", "Whisper", "Blazion", "Shogunyan", "Robonyan"};
-        int random;
-        Random rand = new Random();
-        PlayerTeam playerTeam = new PlayerTeam();
 
-        for (int i = 0; i < 5; i++) {
-            random = rand.nextInt(physicalMoveNames.length);
-            pm = new PhysicalMove(physicalMoveNames[random], "null", 10, 0);
-            random = rand.nextInt(specialMoveNames.length);
-            sm = new SpecialMove(specialMoveNames[random], "null", 10, 0);
-            random = rand.nextInt(ykNames.length);
-            y = new YoKai(ykNames[random], 15, 15, 10, 10, 15, 12, 19, YoKaiTribes.charming, pm, sm);
-            playerTeam.addYoKai(y);
-        }
+        PlayerTeam playerTeam = PlayerSave.getInstance().getPlayerTeam();
         EnemyTeam enemyTeam = new EnemyTeam();
-        for (int i = 0; i < 3; i++) {
-            random = rand.nextInt(physicalMoveNames.length);
-            pm = new PhysicalMove(physicalMoveNames[random], "null", 0, 0);
-            random = rand.nextInt(specialMoveNames.length);
-            sm = new SpecialMove(specialMoveNames[random], "null", 0, 0);
-            random = rand.nextInt(ykNames.length);
-            y = new YoKai(ykNames[random], 5, 5, 10, 10, 15, 12, 19, YoKaiTribes.charming, pm, sm);
-            enemyTeam.addYoKai(y);
-        }
         BattleFrontEnd battleScreen = new BattleFrontEnd(playerTeam, enemyTeam);
     }
 
